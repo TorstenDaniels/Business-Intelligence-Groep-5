c2c17f6cfe5fd776ac40531041aa02e1152d2649
Guust <70886221+Guust@users.noreply.github.com>
2020-11-06 10:48:24 +0100

    :)

--- Guust_dev/R files/dev.R
+++ Guust_dev/R files/dev.R
@@ -7,25 +7,3 @@ library(lubridate)
 library(ggthemes)
 library(rjson)
 
-
-df <- read_csv2("~/GitHub/Business-Intelligence-Groep-5/data/customerSatisfactionBenchmark.csv")
-df$brand <- as.factor(df$brand)
-
-df2 <- read_csv("car_database.csv")
-df2$Brand <- as.factor(df2$Brand)
-df2%>%
-  filter(Brand == "BMW") -> test
-
-write.csv(test, "car_BMW.csv")
-
-
-df%>%
-  group_by(df$year)%>%
-  filter(brand == "BMW" | brand == "Kia")%>%
-  ggplot()+
-  geom_line(aes(year, satisfactionScore, color = brand))+
-  scale_y_continuous(name = "satisfaction score", limits = c(0,100))+
-  theme_economist()
-
-test <- df%>%
-  spread(brand, satisfactionScore)
