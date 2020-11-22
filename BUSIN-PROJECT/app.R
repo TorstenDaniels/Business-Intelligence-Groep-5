a9495fd19510f33f9683d70a6d731924048b064e
Guust <70886221+Guust@users.noreply.github.com>
2020-11-22 09:19:52 +0100

    ???

--- BUSIN-PROJECT/app.R
+++ BUSIN-PROJECT/app.R
@@ -2,6 +2,7 @@
 library(dplyr);library(stringr);library(tidyverse);library(readr);library(ggplot2);library(plotly)
 library(lubridate);library(ggthemes);library(RColorBrewer);library(rworldmap);library(mapproj)
 library(readxl);library(GGally);library(shiny);library(shinydashboard); library(shinydashboardPlus);
+library(naniar)
 
 #loading in data
 source("helpers/Script1.R")
@@ -17,9 +18,9 @@ sidebar <-  dashboardSidebar(
   sidebarMenu(
     id = "nav",
     menuItem("General", tabName = "tab1", icon = icon("dashboard")),
-    menuItem("Market Research", tabName = "tab2"),
-    menuItem("Market Trends", tabName = "tab3"),
-    menuItem("Customer Satisfaction", tabName = "tab4")
+    menuItem("Market Insights", tabName = "tab2", icon = icon("search-plus")),
+    menuItem("Market Trends", tabName = "tab3", icon = icon("chart-line")),
+    menuItem("Customer Satisfaction", tabName = "tab4", icon = icon("smile"))
   )
 )
 
@@ -72,25 +73,62 @@ body <- dashboardBody(
                                 ),
                     status = "primary"
                     ),
-                
-                box(selectInput(inputId = "selectFuelType",
-                                label = "Select Fuel Type",
-                                levels(as.factor(colnames(cars_by_fuel_type[4:9]))),
-                                selected = "Diesel"),
+                box(radioButtons(inputId = "SelectedMarket", 
+                                 label = "Select market",
+                                 c("BMW active markets" = "BMW",
+                                   "BMW non-active markets" = "NoBMW",
+                                   "All" = "All_"),
+                                 inline = T),
+                    height =100,
                     status = "primary"
-                    ),
-    
+                    )
+                ),
+              fluidRow(
                 box("Market share per brand", plotlyOutput("msp_brand", height = 600),
                     status = "primary"),
-                
-                box("Popularity of fuel types per country", plotlyOutput("fueltype_map", height = 600),
-                    status = "primary")
+                box("Sales comparison", plotlyOutput("Sales_comparison", height = 600),
+                    status="primary")
+                ),
+              fluidRow(
+                box(),
+                tabBox(title = "Model per segment", id = "2", height = 500,
+                       tabPanel("Model sales", plotlyOutput("Model_per_segment"),status ="primary"),
+                       tabPanel("Settings", selectInput(inputId = "SelectedSegment",
+                                            label = "Select Segment",
+                                            levels(as.factor(full_segment_sales$type))
+                                            ),
+                                selectInput(inputId = "SelectedYear2",
+                                            label = "Select Year",
+                                            c("2019","2020"),
+                                            selected = "2020")
+              
+                                )
+                       )
                 )
               )
             ),
     #tab 3
     tabItem(tabName = "tab3",
-            h2("test")
+            fluidPage(
+              fluidRow(
+                box(selectInput(inputId = "selectFuelType",
+                                label = "Select Fuel Type",
+                                levels(as.factor(colnames(cars_by_fuel_type[4:9]))),
+                                selected = "Diesel"),
+                    status = "primary"
+                ),
+                box(selectInput(inputId = "SelectedYear_fuel_type", 
+                                label = "Select year", 
+                                levels(as.factor(new_cars_by_fuel_type$Year)), 
+                                selected = "2018",
+                                ),
+                status = "primary"
+                )),
+              fluidRow(
+                box("Popularity of fuel types per country", plotlyOutput("fueltype_map", height = 600),
+                    status = "primary")
+              )
+              )
             ),
     #tab 4
     tabItem(tabName = "tab4")
@@ -105,6 +143,7 @@ ui <- dashboardPagePlus(header, sidebar, body, rightsidebar)
 # Define server logic required to draw graphs
 server <- function(input, output) {
   
+#TAB 1: GENERAL------------------------------------------------------------------------------------------------------------------------  
   output$BMW_market_share <- renderValueBox ({
     comparison_sales_market%>%
       tail(1)%>%
@@ -164,8 +203,12 @@ server <- function(input, output) {
       ggplot(aes(Year,Market.Share)) +
       geom_line() + 
       geom_point() +
-      geom_hline(yintercept = input$target_ms, color = "red") + ylab("% market share")
-    )
+      ylab("% market share")+
+      if (tail(annual_sales_bmw$Market.Share,1)>input$target_ms) 
+        {geom_hline(yintercept = input$target_ms, color = "green")}
+    else
+      {geom_hline(yintercept = input$target_ms, color = "red")}
+      ) 
   })
   
   output$sales_by_segment <- renderPlotly({
@@ -196,19 +239,92 @@ server <- function(input, output) {
         ylab("Sales")
       )
   })
-  
+ 
+#TAB 2: MARKET INSIGHTS----------------------------------------------------------------------------------------------------------------
+   
   output$msp_brand <- renderPlotly({
+    BMW_value <- sales_by_brand%>%
+      filter(Year == input$SelectedYear)%>%
+      filter(Brand == "BMW")%>%
+      select(Sales)
+    
+    FilteredSales <- sales_by_brand %>%
+      filter(Year == input$SelectedYear)
+    
+    SumSales <- sales_by_brand%>%
+      filter(Year == input$SelectedYear)%>%
+      summarise(sum = sum(Sales))
+    
     sales_by_brand%>%
       filter(Year == input$SelectedYear)%>%
-      plot_ly(labels = ~Brand, values= ~Sales)%>%
+      plot_ly(labels = ~Brand, values= ~Sales,textfont=list(size=20))%>%
       add_pie(hole = 0.4)%>%
       layout(xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+             annotations=list(text=paste("BMW: ",round(BMW_value[1,1] / SumSales[1,1] * 100, 2), "%", sep=""), "showarrow"=F, font=list(size = 30, color = "black")))
   })
+  
+  output$Sales_comparison <- renderPlotly({
+    SummarySalesPerSegment <- full_segment_sales %>%
+      group_by(type) %>%
+      summarise(Sales = sum(X2020.H1))
+    
+    SummarySalesPerSegmentBMW <- full_segment_sales %>%
+      filter(str_detect(model,"BMW")) %>%
+      group_by(type) %>%
+      summarise(Sales_BMW = sum(X2020.H1))
+    
+    MarketShareSegments <- SummarySalesPerSegmentBMW %>%
+      right_join(SummarySalesPerSegment)
+    
+    MarketShareSegments[is.na(MarketShareSegments)] <- 0
+    
+    MarketShareSegments <- MarketShareSegments %>%
+      mutate(DeductedSales = Sales-Sales_BMW) %>%
+      select(-Sales)
+    
+    
+    filteredMarketShareSegments <- switch(input$SelectedMarket,
+                   BMW = MarketShareSegments%>%
+                     filter(Sales_BMW>0)%>%
+                     mutate(type = fct_reorder(type, -Sales_BMW)),
+                   NoBMW = MarketShareSegments%>%
+                     filter(Sales_BMW==0)%>%
+                     mutate(type = fct_reorder(type, -DeductedSales)),
+                   All_ = MarketShareSegments%>%
+                     mutate(type = fct_reorder(type, -Sales_BMW)))
+    
+    
+    filteredMarketShareSegments%>%
+      plot_ly(x=~type, y=~Sales_BMW, type='bar', name = "Sales BMW", marker = list(color = "#E7222E"))%>%
+      add_trace(y=~DeductedSales, name ="Rest Of Market Sales", marker = list(color = "#81C4FF"))%>%
+      layout(yaxis = list(title = 'Amount'), barmode = 'stack', legend=list(x=0.7, y=0.9))
+  })
+  
+  output$Model_per_segment <- renderPlotly({ ggplotly(
+      full_segment_sales%>%
+        gather(Year, Sales, -model, -type)%>%
+        filter(type==input$SelectedSegment)%>%
+        filter(if(input$SelectedYear2=="2019"){Year == "X2019.H1"} else {Year=="X2020.H1"})%>%
+        replace_with_na(replace=list(Sales=0))%>%
+        mutate(model = fct_reorder(model, -Sales))%>%
+        drop_na(Sales)%>%
+        head(10)%>%
+        ggplot(aes(model, Sales, fill=factor(ifelse(str_detect(model,"BMW"),"BMW","Others"))))+
+        geom_col()+
+        theme(axis.text.x = element_text(angle = -25, vjust = 1, hjust=1))+
+        scale_fill_manual(name = "model", values=c("#E7222E","#81C4FF"))+
+        ggtitle(paste0("Models in ", input$SelectedSegment))+
+        theme(legend.position='none')
+    , tooltip=c("x","y"))
+    
+  })
+  
+#TAB 3: MARKET TRENDS----------------------------------------------------------------------------------------------------------------
   
   output$fueltype_map <- renderPlotly({
     cars_by_fuel_type%>%
-      filter(Time == input$SelectedYear)%>%
+      filter(Time == input$SelectedYear_fuel_type)%>%
       select(region, Total, Petroleum_Products, LPG, Diesel, Natural_Gas, Electricity, Alternative_Energy)%>%
       gather(key = "FuelType", value = "amount", -region, -Total)%>%
       filter(FuelType == input$selectFuelType)%>%
@@ -223,11 +339,12 @@ server <- function(input, output) {
         scale_fill_gradient(name = "relative percentage of cars", low = "#E7222E", high = "#81C4FF", na.value = "grey50")+
         theme_minimal()+
         theme(axis.text.x = element_blank(),
-            axis.text.y = element_blank(), axis.ticks.x = element_blank(),
-            axis.ticks.y = element_blank(), axis.title = element_blank(),
-            plot.margin = unit(0 * c(-1.5, -1.5, -1.5, -1.5), "lines"))
+              axis.text.y = element_blank(), axis.ticks.x = element_blank(),
+              axis.ticks.y = element_blank(), axis.title = element_blank(),
+              plot.margin = unit(0 * c(-1.5, -1.5, -1.5, -1.5), "lines"))
     )
   })
+  
 }
 
 # Run the application 
